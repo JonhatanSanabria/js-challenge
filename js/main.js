6c81f0ca8f062e5f1fa628719d8d60fcd3318a4b
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2023-07-27 18:57:00 -0600

    prueba

---
+++ js/main.js
@@ -0,0 +1,51 @@
+//Peticion a la base de datos//
+const getPosts = async () => {
+    let response = await fetch("https://proyecto-jonhatan-default-rtdb.firebaseio.com/posts/post1/.json")
+    let data = await response.json()
+    return data
+}
+const testPost = async () => {
+    let posts = await getPosts()
+    console.log(posts)
+}
+getPosts()
+
+
+//crear elementos dom de mi post//
+const createPostCard = () => {
+    let cardCol = document.createElement("")
+    cardCol.classList.add("")
+
+    let cardImage = document.createElement("img")
+    cardImage.classList.add("")
+    cardImage.setAttribute("src", ``)
+    cardImage.setAttribute("alt", "")
+
+    let cardBody = document.createElement("")
+    cardBody.classList.add("")
+
+    let cardUser = document.createElement("")
+    cardUser.classList.add("")
+
+    let imageUser = document.createElement("img")
+    imageUser.classList.add("")
+    imageUser.setAttribute("")
+    imageUser.setAttribute("")
+
+    let authorName = document.createElement("")
+    authorName.classList.add("")
+
+    let postTitle = document.createElement("")
+    postTitle.classList.add("")
+
+    let datePost = document.createElement("")
+    datePost.classList.add("")
+
+    let cardTitle = document.createElement("")
+    cardTitle.classList.add("")
+
+    let contentCard = document.createElement("")
+    contentCard.classList.add("")
+    
+
+}
