d9251dcc9f7ff71926bab26c732a0c5e734cf99f
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2023-07-28 17:50:55 -0600

    fetch

--- js/main.js
+++ js/main.js
@@ -1,18 +1,56 @@
-//Peticion a la base de datos//
-const getPosts = async () => {
+// Peticion a la base de datos //
+const getPostInfo = async () => {
     let response = await fetch("https://proyecto-jonhatan-default-rtdb.firebaseio.com/posts/.json")
     let data = await response.json()
-    console.log(data)
     return data
 }
+
+
+// Convertir objeto con objetos en array de llaves //
 const testPost = async () => {
-    let posts = await getPosts()
-    console.log(posts)
+    let posts = await getPostInfo()
     return posts
 }
 
 
-//crear elementos dom de mi post//
+// Crear info en la base de datos // Enviar info a la base de datos //
+const createPostInfo = async (newPost) => {
+    let response = await fetch("https://proyecto-jonhatan-default-rtdb.firebaseio.com/posts/.json", {
+        method: "POST",
+        body: JSON.stringify(newPost)
+    })
+    let data = await response.json()
+    return data
+}
+createPostInfo() // insertar llaves dentro de la funcion y dentro de las llaves {propiedad:"valor", propiedad:"valor"} //
+// Retorna (key) llave codificada + {propiedad:"valor", propiedad:"valor"}
+
+
+// Obtener Post por Id //
+const getPostById = async (postId) => {
+    let response = await fetch(`https://proyecto-jonhatan-default-rtdb.firebaseio.com/posts/${postId}.json`)
+    // como solo va a obtener información, no es necesario el método de configuración //
+    let data = await response.json()
+    console.log(data)
+    return data
+}
+getPostById() // insertar comillas dentro de la funcion y dentro de las comillas "id de la llave" //
+
+
+// Crear una nueva llave 
+const createNewKey = async (newKey) => {
+    let response = await fetch(`https://proyecto-jonhatan-default-rtdb.firebaseio.com/${newKey}.json`, {
+        method: "PUT",
+        body: JSON.stringify({name: "testing key"})
+    })
+    let data = await response.json()
+    return data
+}
+createNewKey() // insertar comillas dentro de la funcion y dentro de las comillas "nombre que le daremos a la llave" //
+// Pinta en la base de datos nueva info: llave {propiedad:"valor"} : newKey {name:"testing key"} //
+
+
+// Crear elementos dom de mi post //
 const createPostCard = () => {
     let cardCol = document.createElement("")
     cardCol.classList.add("")
