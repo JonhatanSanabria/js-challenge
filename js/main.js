df7a8deea9abfbbd5849bdc867f55a04a08c5d78
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2023-07-28 22:30:26 -0600

    form

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const getPostInfo = async () => {
 // Convertir objeto con objetos en array de llaves //
 const testPost = async () => {
     let posts = await getPostInfo()
+    console.log(posts)
     return posts
 }
 
@@ -87,5 +88,79 @@ const createPostCard = () => {
     let contentCard = document.createElement("")
     contentCard.classList.add("")
     
-
 }
+
+
+// Boton switcheable //         id en etiqueta del boton html
+let myButton = document.getElementById("some-button")
+
+myButton.addEventListener("click", (event) => {
+    console.log(event)
+    console.log(event.target)
+    event.target.classList.toggle("active")
+    event.target.classList.contains("active") 
+    ? (event.target.innerText = "Desactivar") 
+    : (event.target.innerText = "Activar")
+})
+
+
+// boton para ditigir a otro html // 
+let navButton = document.getElementById("navigate-button").addEventListener("click", () => {
+    window.open("url del formulario")
+})
+
+
+// Hacer que se impriman los comentarios ( o posts ) //
+let comments = [
+    {
+        author: "Jonhatan",
+        comment: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    }
+]
+// Funcion para crear las cards //
+const createCommentBox = (commentData) => {  // el parametro trabaja solo dentro del bloque de codigo //
+    let { author, comment } = commentData 
+    let card = document.createElement("div") // en caso de que sea "div" //
+    card.classList.add("card")  // la clase de la etiqueta "div" seria "card" //
+
+    let cardBody = document.createElement("div") // en caso de que sea "div" //
+    cardBody.classList.add("card-body") // la clase de la etiqueta "div" seria "card-body" //
+
+    let cardText = document.createTextNode(`${author} : ${comment}`)
+
+    cardBody.append(cardText)
+    card.append(cardBody)
+
+    return card
+}
+// Funcion para imprimir todos los comments ( o posts ) //
+const printAllComments = (commentsArray) => {
+    let wrapper = document.getElementById("comment-wrapper") // esta sera la clase del "div" padre //
+    wrapper.innerHTML = ""
+
+    commentsArray.forEach((comment) => {
+        let commentBox = createCommentBox(comment)
+        wrapper.append(commentBox)        
+    });
+}
+printAllComments(comments)
+// Agregar un commentario desde el formulario ( actuando con <input> ) //
+let commentObject = {}
+// Todos los campos de formulario tienen una propiedad que se llama "name" //
+document.querySelectorAll("#comment-form input").forEach((input) => {
+    input.addEventListener("keyup", (event) => {
+        let value = event.target.value
+        let property = event.target.name
+        console.log(`${property}:${value}`)
+        commentObject[property] = value
+        console.log(commentObject)
+    })
+}) // Con esto, todo lo que se escriba en los inputs se ira guardando en el array de objetos de "commentObject" con el
+// --nombre de la llave con el texto como valor de ese campo //
+
+// Agregar los commentObject al array con el boton submit del formulario //
+document.getElementById("save-comment").addEventListener("click", () => {
+    comments.push(commentObject)
+    console.log(comments)
+    printAllComments(comments)
+}) 
