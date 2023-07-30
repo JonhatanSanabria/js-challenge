f6af66cb822470ac0c77d8b60fabdee02993a47d
Victor Nesta <vic842009@gmail.com>
2023-07-30 11:53:14 -0600

    add: token

--- js/login.js
+++ js/login.js
@@ -1,3 +1,6 @@
+
+ 
+
 console.log("hola")
 
 let user = {};
@@ -10,5 +13,17 @@ textFields.forEach((field)=>{
         let property = event.target.name;
         let value = event.target.value;
         user[property]=value
+        console.log(user);
     })
 })
+
+
+document.querySelector("#login-button").addEventListener("click", () => {
+    let saveUser = textFields(user);
+    if(user.name) {
+        localStorage.setItem(
+            "token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        );
+        window.open("index.html", "_self")
+    }
+});
