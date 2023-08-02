3940c88fd2332f96bdc40f2e169ac296a716a7e1
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2023-08-01 21:56:18 -0600

    test merge

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Peticion a la base de datos //
+/// Peticion a la base de datos ///
 const getPostInfo = async () => {
     let response = await fetch("https://proyecto-jonhatan-default-rtdb.firebaseio.com/posts/.json")
     let data = await response.json()
@@ -52,45 +52,6 @@ createNewKey() // insertar comillas dentro de la funcion y dentro de las comilla
 // Si la funcion se ejecuta sin contenido, aparecera: undefined {propiedad: "valor"} : undefined {name:"testing key"} //
 
 
-// Crear elementos dom de mi post //
-const createPostCard = () => {
-    let cardCol = document.createElement("")
-    cardCol.classList.add("")
-
-    let cardImage = document.createElement("img")
-    cardImage.classList.add("")
-    cardImage.setAttribute("src", ``)
-    cardImage.setAttribute("alt", "")
-
-    let cardBody = document.createElement("")
-    cardBody.classList.add("")
-
-    let cardUser = document.createElement("")
-    cardUser.classList.add("")
-
-    let imageUser = document.createElement("img")
-    imageUser.classList.add("")
-    imageUser.setAttribute("")
-    imageUser.setAttribute("")
-
-    let authorName = document.createElement("")
-    authorName.classList.add("")
-
-    let postTitle = document.createElement("")
-    postTitle.classList.add("")
-
-    let datePost = document.createElement("")
-    datePost.classList.add("")
-
-    let cardTitle = document.createElement("")
-    cardTitle.classList.add("")
-
-    let contentCard = document.createElement("")
-    contentCard.classList.add("")
-    
-}
-
-
 // Boton switcheable //         id en etiqueta del boton html
 let myButton = document.getElementById("some-button")
 
@@ -103,64 +64,152 @@ myButton.addEventListener("click", (event) => {
     : (event.target.innerText = "Activar")
 })
 
-
 // boton para ditigir a otro html // 
 let navButton = document.getElementById("navigate-button").addEventListener("click", () => {
     window.open("url del formulario")
 })
 
 
+/*
+<div class="card mb-3"><!--Container-->
+            <img src="./assets/images/final.webp" class="card-img-top" alt="imagen del post" ><!--Image-->
+            <div class="card-body"><!--Form-->
+              <div class="d-flex flex-row"><!--Body-->
+                <img class="rounded-circle icon__image" width="40px" height="40px" src="./assets/images/icon2.jpeg" alt="icon"><!--User image-->
+                <div class="text ms-2"><!--User Body-->
+                <h6 class="m-0">{author}</h6><!--Author-->
+                <span class="date">{date}</span><!--Date-->
+                </div>
+              </div>
+              <h5 class="card-title mt-2">{title}</h5><!--title-->
+              <div class="d-flex flex-row justify-content-between"><!--Tags Body--> 
+                <div class="d-flex flex-row"><!--Tags Wrapper-->
+                  <p class="card-text">{tags}</p><!--Tags-->
+                  <div class="d-block">{reactions}<!--Reactions-->
+                    <h6 class="content-box">{content}</h6><!--Content-->
+                  </div>
+                </div>
+              </div>        
+            </div>
+  
+          </div>
+*/
 // Hacer que se impriman los comentarios ( o posts ) //
-let comments = [
+let posts = [
     {
-        author: "Jonhatan",
-        comment: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        author: "John Walker",
+        content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, soluta quas! Deleniti doloribus quasi voluptas mollitia excepturi sunt, autem ullam, totam ipsum maiores necessitatibus accusantium nam iste perspiciatis, nobis eos!",
+        date: "9 July",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcoder&psig=AOvVaw2g-2kBJWycVFPFm5bYknVA&ust=1690575301125000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjEjPXZr4ADFQAAAAAdAAAAABAE",
+        reaction: "🔥🙌🦄",
+        tags: "#javascript",
+        title: "Inthe name of the Koders"
     }
 ]
-// Funcion para crear las cards //
-const createCommentBox = (commentData) => {  // el parametro trabaja solo dentro del bloque de codigo //
-    let { author, comment } = commentData 
-    let card = document.createElement("div") // en caso de que sea "div" //
-    card.classList.add("card")  // la clase de la etiqueta "div" seria "card" //
+/// Funcion para crear los posts ///
+const createPostBox = (postData) => {  // el parametro trabaja solo dentro del bloque de codigo //
+    let { author, content, date, image, reaction, tags, title } = postData 
+    let post = document.createElement("div") // Container //
+    post.classList.add("card mb-3")
 
-    let cardBody = document.createElement("div") // en caso de que sea "div" //
-    cardBody.classList.add("card-body") // la clase de la etiqueta "div" seria "card-body" //
+    let postImage = document.createElement("img") // Image //
+    postImage.classList.add("card-img-top")
+    postImage.setAttribute("src", "./assets/images/final.webp")
+    postImage.setAttribute("alt", "imagen del post")
 
-    let cardText = document.createTextNode(`${author} : ${comment}`)
+    let postForm = document.createElement("div") // Form //
+    postForm.classList.add("card-body")
 
-    cardBody.append(cardText)
-    card.append(cardBody)
+    let postBody = document.createElement("div") // Body //
+    postBody.classList.add("d-flex", "flex-row")
 
-    return card
+    let userImage = document.createElement("img") // User Image //
+    userImage.classList.add("rounded-circle", "icon__image")
+    userImage.setAttribute("src", "./assets/images/icon2.jpeg")
+    userImage.setAttribute("alt", "icon")
+
+    let wrapperText = document.createElement("div") // User Body //
+    wrapperText.classList.add("text", "ms-2")
+
+    let authorName = document.createElement("h6") // Author //
+    authorName.classList.add("m-0")
+    authorName.innerText = "John Walker"
+
+    let creationDate = document.createElement("span") // Date //
+    creationDate.classList.add("date")
+    creationDate.innerText = "9 July"
+
+    let titleLine = document.createElement("h5") // Title //
+    titleLine.classList.add("card-title", "mt-2")
+    titleLine.innerText = "In the name of the Koders"
+
+    let tagsBody = document.createElement("div") // Tags Body //
+    tagsBody.classList.add("d-flex", "flex-row", "justify-content-between")
+
+    let tagsWrapper = document.createElement("div") // Tags Wrapper //
+    tagsWrapper.classList.add("d-flex", "flex-row")
+
+    let tagsLine = document.createElement("p") // Tags //
+    tagsLine.classList.add("card-text")
+    tagsLine.innerText = "#javascript"
+
+    let reactionBody = document.createElement("div") // Reactions //
+    reactionBody.classList.add("d-block")
+    reactionBody.innerText = "🔥🙌🦄❤️"
+
+    let contentBox = document.createElement("h6") // Content //
+    contentBox.classList.add("content-box")
+    contentBox.innerText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, soluta quas! Deleniti doloribus quasi voluptas mollitia excepturi sunt, autem ullam, totam ipsum maiores necessitatibus accusantium nam iste perspiciatis, nobis eos!"
+
+    reactionBody.append(contentBox)
+    tagsWrapper.append(reactionBody, tagsLine)
+    tagsBody.append(tagsWrapper)
+
+    wrapperText.append(authorName, creationDate)
+    postBody.append(userImage, wrapperText)
+
+    postForm.append(postBody, titleLine, tagsBody)
+    post.append(postImage, postForm)
+
+    return post
 }
-// Funcion para imprimir todos los comments ( o posts ) //
-const printAllComments = (commentsArray) => {
-    let wrapper = document.getElementById("comment-wrapper") // esta sera la clase del "div" padre //
+let cardText = document.createTextNode(`${author} : ${comment}`)
+
+
+// Funcion para imprimir todos los posts ( o posts ) //
+const printAllposts = (posts) => {
+    let wrapper = document.getElementById("post-wrapper") // esta sera la clase del "div" padre //
     wrapper.innerHTML = ""
 
-    commentsArray.forEach((comment) => {
-        let commentBox = createCommentBox(comment)
-        wrapper.append(commentBox)        
+    posts.forEach((post) => {
+        let postBox = createPostBox(post)
+        wrapper.append(postBox)        
     });
 }
-printAllComments(comments)
+printAllposts(posts)
 // Agregar un commentario desde el formulario ( actuando con <input> ) //
-let commentObject = {}
+let postObject = {}
 // Todos los campos de formulario tienen una propiedad que se llama "name" //
-document.querySelectorAll("#comment-form input").forEach((input) => {
+document.querySelectorAll("#post-form input").forEach((input) => {
     input.addEventListener("keyup", (event) => {
         let value = event.target.value
         let property = event.target.name
         console.log(`${property}:${value}`)
-        commentObject[property] = value
-        console.log(commentObject)
+        postObject[property] = value
+        console.log(postObject)
     })
-}) // Con esto, todo lo que se escriba en los inputs se ira guardando en el array de objetos de "commentObject" con el
+}) // Con esto, todo lo que se escriba en los inputs se ira guardando en el array de objetos de "postObject" con el
 // --nombre de la llave con el texto como valor de ese campo //
+const resetForm = (formId) => {   // Haremos que se borren los campos del formulario cuando demos click en el boton // 
+    let fields = document.querySelectorAll(`#${formId} input`)
+    fields.forEach((field) => (field.value = ""));
+}
 
-// Agregar los commentObject al array con el boton submit del formulario //
+// Agregar los postObject al array con el boton submit del formulario //
 document.getElementById("save-comment").addEventListener("click", () => {
-    comments.push(commentObject)
-    console.log(comments)
-    printAllComments(comments)
-}) 
+    posts.push(postObject)
+    console.log(posts)
+    printAllposts(posts)
+    resetForm("post-form")
+    postObject = {}   
+})  // "posts" es el nombre de la variable que contendra el array de objetos donde se ira guardando cada card //
