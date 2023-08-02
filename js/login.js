ee4afda6e516e1e410f9ff3cf26928a780f0ee69
Victor Nesta <vic842009@gmail.com>
2023-08-01 19:46:16 -0600

    add: button Create account

--- js/login.js
+++ js/login.js
@@ -51,6 +51,11 @@ let buttonPurple = document.createElement("button");
 buttonPurple.classList.add("btn", "button__primary--purple", "d-none", "d-sm-none", "d-md-block");
 buttonPurple.innerText = "login";
 
+let buttonCount = document.createElement("button")
+buttonCount.classList.add("btn", "btn-outline-primary")
+buttonCount.setAttribute("type", "button")
+buttonCount.innerText = "Create account"
+
 let buttonNavar = document.createElement("button");
 buttonNavar.classList.add("btn", "navbar__buttton--transparent", "button__primary--transparent-purple", "ms-2");
 
@@ -72,7 +77,7 @@ aImage.append(navImage);
 
 divConteiner.append(aImage, formdiv, buttonCamvas, divDflex);
 
-divDflex.append(buttonPurple, buttonNavar);
+divDflex.append(buttonPurple, buttonNavar, buttonCount);
 
 navConteiner.append(divConteiner);
 
@@ -88,47 +93,6 @@ navId.append(navConteiner);
 
 
 
-
-
-
-
-
-
-    // <nav class="navbar navbar-expand-lg navbar__container--white">
-    //     <div class="container">
-    //         <div class="d-flex justify-content-start align-items-center">
-    //             <button class="btn d-block d-sm-block d-md-none" data-bs-toggle="offcanvas" href="#offcanvasExample">
-    //                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-labelledby="amizgsgt7gjk1tltjcnt8x0qk5buzpwe" class="crayons-icon"><title id="amizgsgt7gjk1tltjcnt8x0qk5buzpwe">Navigation menu</title>
-    //                     <path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"></path>
-    //                 </svg>
-    //             </button>
-    //             <a class="navbar-brand" href="./index.html">
-    //               <img
-    //                 class="img-fluid navbar__logo"
-    //                 src="./assets/images/dev-to-logo.png"
-    //                 alt="Logo"
-    //               />
-    //             </a>
-    //             <form
-    //               class="d-flex justify-content-start navbar__search-container d-none d-sm-none d-md-block"
-    //               role="search">
-    //                 <div class="input-group">
-    //                 <input
-    //                   class="form-control rounded"
-    //                   type="text"
-    //                   placeholder="Search..."
-    //                   id="example-search-input"/>                    
-    //                 </div>
-    //             </form>
-    //         </div>
-    //             <div class="d-flex navbar__container-right align-items-center">                
-    //             <button class="btn button__primary--purple d-none d-sm-none d-md-block">Create Post</button>
-    //             <button class="btn navbar__buttton--transparent button__primary--transparent-purple ms-2"></button>                
-    //             </div>
-    //     </div>
-    // </nav>
-
- 
 
 
 let user = {};
